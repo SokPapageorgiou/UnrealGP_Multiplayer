bf35aa0c384b7ba4ab607da6a464de69e2c3a5c8
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-10-14 10:09:09 +0200

    Add Server Interact

--- Source/UnrealGP/UnrealGP.Build.cs
+++ Source/UnrealGP/UnrealGP.Build.cs
@@ -8,7 +8,16 @@ public class UnrealGP : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "NavigationSystem", "Inventory" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"AIModule", 
+			"NavigationSystem", 
+			"Inventory"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
