b69969bff7f39af458b11e33b887111c8cc161a9
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-10-15 14:02:48 +0200

    BugFix Interface

--- Plugins/Inventory/Source/Inventory/Private/CharacterBase.cpp
+++ Plugins/Inventory/Source/Inventory/Private/CharacterBase.cpp
@@ -35,7 +35,7 @@ void ACharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 
 void ACharacterBase::ServerInteract_Implementation(AActor* HitActor, ACharacter* Interactor)
 {
-	if (!UKismetSystemLibrary::DoesImplementInterface(HitActor, UInterface::StaticClass())) return;
+	//if (!UKismetSystemLibrary::DoesImplementInterface(HitActor, UInterface::StaticClass())) return;
 	
 	IInteractIF::Execute_Interact(HitActor, Interactor);	
 }
